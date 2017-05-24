34d4dbaace6848c010c4f41e81241057a25a3abc
Faunie <faunie.szeloczei@gmail.com>
2017-05-23 21:47:26 -0700

    create a functioning that allows you to change the color or your backgrount

--- js/scripts.js
+++ js/scripts.js
@@ -20,3 +20,20 @@ $(document).ready(function() {
     $("#cow-hidden").toggle();
   });
 });
+
+$(document).ready(function() {
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+});
